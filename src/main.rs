f20c43fe17837352e1dd5feeafc00ef866052e44
avidlaud <avidlaud@gmail.com>
2022-03-15 19:54:30 -0400

    Add SHA hashing

--- src/main.rs
+++ src/main.rs
@@ -2,15 +2,22 @@ mod canvas;
 mod action;
 
 use std::fs;
+use sha3::{Digest, Sha3_512};
 
 use crate::canvas::Grid;
 
 fn main() {
-    let mut canvas = canvas::Canvas::new(canvas::FlatGrid::new(17, 9));
+    let mut canvas = canvas::Canvas::new(canvas::FlatGrid::new(5, 5));
 
     let bytes = fs::read("./key.gpg").unwrap();
 
-    let actions : Vec<action::Action> = bytes.iter()
+    let mut hasher = Sha3_512::new();
+
+    hasher.update(bytes);
+
+    let hash = hasher.finalize();
+
+    let actions : Vec<action::Action> = hash.iter()
         .flat_map(|b| [b >> 4, b & 0x0F])
         .map(action::get_action)
         .collect();
